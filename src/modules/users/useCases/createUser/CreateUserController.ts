744741005bbbab279b61ffc48041e281a777a391
Jack <Jackson.contato24@hotmail.com>
2022-02-03 16:31:29 -0300

    End project

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,19 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body;
+
+      if (!name || !email) {
+        return response.status(400).send({ error: "Email ou nome inválidos" });
+      }
+
+      const user = this.createUserUseCase.execute({ name, email });
+
+      return response.status(201).json(user);
+    } catch (err) {
+      return response.status(400).send({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,14 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+  user_id: string | string[];
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(
+      typeof user_id === "string" ? user_id : user_id[0]
+    );
+
+    if (!user || !user.admin) {
+      throw new Error("User does't exists or don't have privileges");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
